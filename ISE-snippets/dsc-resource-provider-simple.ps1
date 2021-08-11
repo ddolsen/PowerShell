26833f15ed21d1269cffae63f128850ac67b01c5
ddolsen <32841888+ddolsen@users.noreply.github.com>
2021-08-10 22:32:06 -0500

    Create dsc-resource-provider-simple.ps1

---
+++ ISE-snippets/dsc-resource-provider-simple.ps1
@@ -0,0 +1,23 @@
+Function Get-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param(
+    )
+}
+
+Function Set-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param(
+    )
+}
+
+Function Test-TargetResource {
+    # TODO: Add parameters here
+    # Make sure to use the same parameters for
+    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
+    param(
+    )
+}
