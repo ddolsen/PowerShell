802677f9467ce15ca76bac499223c597501fb275
ddolsen <32841888+ddolsen@users.noreply.github.com>
2021-08-26 22:03:05 -0500

    Update open-page-and-refresh-every-x.ps1

--- stack-overflow/open-page-and-refresh-every-x.ps1
+++ stack-overflow/open-page-and-refresh-every-x.ps1
@@ -1,3 +1,5 @@
+# https://stackoverflow.com/questions/68922453/new-to-programming-it-should-refresh-the-site-but-it-wont-thanks/68923344#68923344
+
 $proc = [System.Diagnostics.Process]::Start("chrome.exe", "https://www.stackoverflow.com") | Get-Process
 $wshell = New-Object -ComObject WScript.Shell
 
