92e62aa97efec831205955f602015290086480ea
ddolsen <32841888+ddolsen@users.noreply.github.com>
2021-09-01 14:16:09 -0500

    Update node.js-dl-install-run-at-logon.ps1

--- stack-overflow/node.js-dl-install-run-at-logon.ps1
+++ stack-overflow/node.js-dl-install-run-at-logon.ps1
@@ -1,3 +1,10 @@
+Use Invoke-WebRequest to download the Node.js Windows installer
+# Use Start-Process and msiexec.exe to install it
+# With Node.js installed, use npm to install the latest version of PM2
+# Use Invoke-WebRequest again to download the Node app file
+# Create a scheduled task that runs at logon with New-JobTrigger and Register-ScheduledJob
+# https://stackoverflow.com/questions/68100669/how-to-make-a-windows-10-install-file/68106369#68106369
+
 Invoke-WebRequest -Uri "https://nodejs.org/dist/v14.17.1/node-v14.17.1-x64.msi" -OutFile ".\node-v14.17.1-x64.msi"
 Start-Process msiexec.exe -Wait -ArgumentList "/i node-v14.17.1-x64.msi /quiet"
 
